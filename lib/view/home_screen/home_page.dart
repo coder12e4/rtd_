bb78b0e95eae3bc9957f73c3488086a19b0a788e
Sreenivas shenoy v <sreenivasshenoy20@gmail.com>
2023-08-11 11:15:45 +0530

    Daily commit3

--- lib/view/home_screen/home_page.dart
+++ lib/view/home_screen/home_page.dart
@@ -30,7 +30,7 @@ class _NavigationBarpageState extends State<NavigationBarpage> {
       body: Stack(
         children: [
           
-
+          
           Padding(
             padding: const EdgeInsets.all(30.0),
             child: Align(
